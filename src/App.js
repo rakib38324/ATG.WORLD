52e41a579e05ebdeedf73519a5316e66f73ebd07
rakib38324 <rakib38324@gmail.com>
2023-02-15 02:08:54 +0600

    Nabvar setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-// import Button from 'react-bootstrap/Button';
-import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routers/Routers';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="outline-primary">Primary</Button>{' '}
-      <Button variant="outline-secondary">Secondary</Button>{' '}
-      <Button variant="outline-success">Success</Button>{' '}
-      <Button variant="outline-warning">Warning</Button>{' '}
-      <Button variant="outline-danger">Danger</Button>{' '}
-      <Button variant="outline-info">Info</Button>{' '}
-      <Button variant="outline-light">Light</Button>{' '}
-      <Button variant="outline-dark">Dark</Button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
